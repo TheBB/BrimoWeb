c6bc15b9de04b124ce12b1c543be68192cabdab7
Eivind Fonn <evfonn@gmail.com>
2020-07-04 09:13:15 +0200

    Initial commit

---
+++ brimoweb/local_settings.template.py
@@ -0,0 +1,7 @@
+# Copy this file to local_settings.py and fill in the below settings
+
+# A random string of suitable length
+SECRET_KEY = ''
+
+# Set to False in production, True in development
+DEBUG = True
